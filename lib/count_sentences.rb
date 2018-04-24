27c3e4312654f1ed135467e36d93837d2613527e
Cat Norris <cannenorris@yahoo.com>
2018-04-24 05:35:12 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,17 +3,17 @@ require 'pry'
 class String
 
   def sentence?
-     self.end_with?(".")  
+     self.end_with?(".")
   end
 
   def question?
     self.end_with?("?")
-    
+
   end
 
   def exclamation?
      self.end_with?("!")
-      
+
   end
 
   def count_sentences
